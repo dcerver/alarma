6c311cc8ba16c1b5fceb5f00077b456d26f32f0e
dcerver <121509667+dcerver@users.noreply.github.com>
2023-04-27 09:21:38 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,25 @@
-radio.setGroup(76)
-music.setVolume(31)
-basic.forever(function () {
-    if (input.magneticForce(Dimension.Strength) > 150) {
-        music.stopAllSounds()
-        radio.sendNumber(0)
-    } else {
-        radio.sendNumber(1)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-        basic.pause(100)
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "ON") {
+        if (input.magneticForce(Dimension.Strength) > 150) {
+            music.stopAllSounds()
+            radio.sendNumber(0)
+        } else {
+            radio.sendNumber(1)
+            music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.InBackground)
+            basic.showLeds(`
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                # # # # #
+                `)
+            basic.pause(100)
+            basic.clearScreen()
+            basic.pause(100)
+        }
+    } else if (receivedString == "OFF") {
+        basic.showIcon(IconNames.No)
     }
 })
+radio.setGroup(76)
+music.setVolume(31)
